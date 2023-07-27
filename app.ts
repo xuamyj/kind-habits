fdf9ef1e7eb500265d353966a0778c47e7515631
Amy Xu <xuamyj@gmail.com>
2023-07-27 19:37:15 -0400

    npm install @supabase/supabase-js + notes, while creating user + tables in supabase dashboard

--- app.ts
+++ app.ts
@@ -12,6 +12,30 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html'); 
     res.end(html);
+
+  // view all boards for [user]: GET
+  // create new [board] for [user]: POST
+
+  // view board info for [board]: GET
+  // edit board info for [board]: POST - check board exists?
+  // delete board for [board]: POST - check board exists? 
+  
+  // view current month of days on [board]: GET
+  // view [month] of days on [board]: GET
+  // create new [day] on [board]: POST
+
+  // view day for [day]: GET
+  // edit day for [day]: POST - check day exists? 
+  // delete day for [day]: POST - check day exists? 
+  
+  // view profile: GET
+  // create profile: POST
+  // edit profile: POST
+  
+  // create account: POST
+  // login: POST
+  // logout: POST
+
   } else if (req.url === "/other") { 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
