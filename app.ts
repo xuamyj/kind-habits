7ad75ecd9ad02e3bb748c3098a8570bbfc5ba597
Amy Xu <xuamyj@gmail.com>
2023-07-26 22:24:51 -0400

    two urls, one to index.html

--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
 // https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
 // and `npm install --save @types/node`
+import { readFileSync } from 'fs';
 import * as http from 'http'; 
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  if (req.url === "/") {  
+    const html = readFileSync('index.html');  
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html'); 
+    res.end(html);
+  } else if (req.url === "/other") { 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+  } 
 });
 
 server.listen(port, hostname, () => {
